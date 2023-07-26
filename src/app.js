cf892c20d4a9b89f2dd134e4c5a5c67627669578
niv3D <niv3d.username@gmail.com>
2023-07-26 23:29:52 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const routes = require('./routes');
 
 const app = express();
 app.use(morgan('dev'));
